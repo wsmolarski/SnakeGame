27f1e3b09c7f7698b2de85996e96205b09ea6cee
wsmolarski <wmsmolarski@gmail.com>
2023-12-30 18:44:53 +0100

    html, css and some scripts finished

---
+++ script.js
@@ -0,0 +1,116 @@
+let dom_replay = document.querySelector("#replay");
+let dom_score = document.querySelector("#score");
+let dom_canvas = document.createElement("canvas");
+document.querySelector("#canvas").appendChild
+(dom_canvas);
+let CTX = dom_canvas.getContext("2d");
+
+const W = (dom_canvas.width = 500);
+const H = (dom_canvas.height = 500);
+
+let snake, food, currentHue, cells = 20, cellSize,
+isGameOver = false, tails = [], score = 0o0, maxScore = 
+window.localStorage.getItem("maxScore") || undefined,
+particles = [], splashingParticleCount = 20, cellsCount,
+requestID;
+
+let helpers = {
+    Vec: class{
+        constructor(x, y){
+            this.x = x;
+            this.y = y;
+        }
+        add(v){
+            this.x += v.x;
+            this.y += v.y;
+            return this;
+        }
+        mult(v){
+            if(v instanceof helpers.Vec){
+                this.x *= v.x;
+                this.y *= v.y;
+                return this;
+            }else{
+                this.x *= v;
+                this.y *= v;
+                return this;
+            }
+        }
+    },
+    isCollision(v1, v2){
+        return v1.x == v2.x && v1.y == v2.y;
+    },
+    garbageCollector(){
+        for(let i = 0; i < particles.length; i++){
+            if(particles[i].size <= 0){
+                particles.splice(i, 1);
+            }
+        }
+    },
+    drawGrid(){
+        CTX.lineWidth = 1.1;
+        CTX.strokeStyle = "#181825";
+        CTX.shadowBlur = 0;
+        for(let i = 1; i < cells; i++){
+            let f = (W / cells) * i;
+            CTX.beginPath();
+            CTX.moveTo(f, 0);
+            CTX.lineTo(f, H);
+            CTX.stroke();
+            CTX.beginPath();
+            CTX.moveTo(0, f);
+            CTX.lineTo(W, f);
+            CTX.stroke();
+            CTX.closePath();
+        }
+    },
+    randHue(){
+        return ~~(Math.random() * 360);
+    },
+    hs12rgb(hue, saturation, lightness){
+        if(hue == undefined){
+            return[0, 0, 0];
+        }
+        var chroma = 1 (1 - Math.abs(2 * lightness - 1))*
+        saturation;
+        var huePrime = hue / 60;
+        var secondComponent = chroma * (1 - Math.abs
+        ((huePrime % 2) - 1));
+
+        huePrime = ~~huePrime;
+        var red;
+        var green;
+        var blue;
+
+        if(huePrime === 0){
+            red = chroma;
+            green = secondComponent;
+            blue = 0;
+        }else if(huePrime === 1){
+            red = secondComponent;
+            green = chroma;
+            blue = 0;
+        }else if(huePrime === 2){
+            red = 0;
+            green = chroma;
+            blue = secondComponent;
+        }else if(huePrime === 3){
+            red = 0;
+            green = secondComponent;
+            blue = chroma;
+        }else if(huePrime === 4){
+            red = secondComponent
+            green = 0;
+            blue = chroma;
+        }else if(huePrime === 5){
+            red = chroma;
+            green = 0;
+            blue = secondComponent;
+        }
+
+        var lightnessAdjustment = lightness - chroma /2;
+        red += lightnessAdjustment;
+        green += lightnessAdjustment;
+        blue += lightnessAdjustment;
+    }
+}
